576b0e16d519c43a9402b95f9974cf13a033ceb3
Meikelele <michal.bak2002@interia.pl>
2026-02-28 22:21:16 +0100

    chore(engine): add SimulationEngine skeleton

--- engine/src/main/java/pl/mbak/traffic/engine/simulation/SimulationEngine.java
+++ engine/src/main/java/pl/mbak/traffic/engine/simulation/SimulationEngine.java
@@ -1,2 +1,12 @@
-package pl.mbak.traffic.engine.simulation;public class SimulationEngine {
+package pl.mbak.traffic.engine.simulation;
+
+import pl.mbak.traffic.engine.io.SimulationInput;
+import pl.mbak.traffic.engine.io.SimulationOutput;
+
+public final class SimulationEngine {
+
+    public SimulationOutput simulate(SimulationInput simulationInput) {
+        // TODO: implement logic one day
+        return SimulationOutput.empty();
+    }
 }
