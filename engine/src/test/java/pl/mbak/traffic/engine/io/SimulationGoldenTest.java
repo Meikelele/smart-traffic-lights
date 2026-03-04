fa15ac51f5fa9471365fa9b02ab3f63078cb4a1c
Meikelele <michal.bak2002@interia.pl>
2026-03-04 16:33:20 +0100

    test(engine): add unit test for Turns.compute & changed output_expected.json file for SimulationGoldenTest

--- engine/src/test/java/pl/mbak/traffic/engine/io/SimulationGoldenTest.java
+++ engine/src/test/java/pl/mbak/traffic/engine/io/SimulationGoldenTest.java
@@ -1,24 +1,21 @@
 package pl.mbak.traffic.engine.io;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pl.mbak.traffic.engine.simulation.SimulationEngine;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimulationGoldenTest {
     @Test
-    @Disabled("Pending: implement SimulationEngine logic")
     void shouldMatchExpectedOutputForSample() throws Exception {
         Path inputPath = Path.of("../samples/input_sample.json");
-        Path expectedPath = Path.of("../samples/output_sample.json");
+        Path expectedPath = Path.of("../samples/output_expected.json");
 
         assertTrue(Files.exists(inputPath), "Missing: " + inputPath.toAbsolutePath());
         assertTrue(Files.exists(expectedPath), "Missing: " + expectedPath.toAbsolutePath());
