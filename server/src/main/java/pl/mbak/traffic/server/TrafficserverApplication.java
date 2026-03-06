2699cbb680da681120d668424bd40c61fc3c4173
Meikelele <michal.bak2002@interia.pl>
2026-03-06 15:09:55 +0100

    feat(server): add Spring Boot server module

---
+++ server/src/main/java/pl/mbak/traffic/server/TrafficserverApplication.java
@@ -0,0 +1,13 @@
+package pl.mbak.traffic.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficserverApplication.class, args);
+    }
+}
