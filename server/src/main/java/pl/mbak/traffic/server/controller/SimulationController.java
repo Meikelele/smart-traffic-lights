fa3ac704acb6b61ff6f421a1475d2b08c8431d0e
Meikelele <michal.bak2002@interia.pl>
2026-03-06 15:15:28 +0100

    chore(web): angular init

--- server/src/main/java/pl/mbak/traffic/server/controller/SimulationController.java
+++ server/src/main/java/pl/mbak/traffic/server/controller/SimulationController.java
@@ -2,11 +2,8 @@ package pl.mbak.traffic.server.controller;
 
 import org.springframework.web.bind.annotation.*;
 import pl.mbak.traffic.engine.io.SimulationInput;
-import pl.mbak.traffic.engine.io.SimulationOutput;
 import pl.mbak.traffic.engine.simulation.SimulationEngine;
 import pl.mbak.traffic.engine.trace.SimulationTrace;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/simulate")
